1fea09dec7cc3677af31cf5d42594f26cfce0aa1
Archangel0007 <krishnannavaneetha467@gmail.com>
2023-09-09 20:08:31 +0530

    Brand_Page Creation

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Nav from "./Nav.jsx"
+import Content from "./Content.jsx"
+const App = () => {
+  return (
+    <div className="Poster">
+      <Nav/>
+      <Content/>
+    </div>
+  )
+}
+export default  App;
---
+++ vite-project/src/Content.jsx
@@ -0,0 +1,25 @@
+const Content = () => {
+    return (
+    <div className="Content">
+    <div className="WContent">
+      <div className="Moto">
+        YOUR FEET DESERVE THE BEST
+      </div>
+      <div className="description">
+      YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+      </div>
+      <div className="Buttons">
+        <button className="ShopNow">Shop Now</button>
+        <button className="Category">Category</button>
+      </div>
+      <div className="links">
+      Also Available On
+      </div>
+      <img src="/images/shops.png" alt="Shoe"/>
+    </div>
+    <div>
+      <img className="Shoeimage" src="/images/shoe_image.png" alt="" />s
+    </div>
+  </div>)
+}
+export default Content;
